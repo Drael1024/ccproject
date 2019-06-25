c384969e24aaeb219f92b9450c82a47ff09e8020
Kuczora Tibor <kuczora.tibi@gmail.com>
2019-06-25 06:49:49 -0700

    Added file: tictactoe.py + Creating the game table + showTable(): Prints out the table.

---
+++ tictactoe.py
@@ -0,0 +1,8 @@
+table = [' ' for x in range(10)] # Creating the table for the game. (Note: We won't use the 0th element!!!)
+
+def showTable():
+    print(' ' + table[1] + ' | ' + table[2] + ' | ' + table[3])
+    print('-----------')
+    print(' ' + table[4] + ' | ' + table[5] + ' | ' + table[6])
+    print('-----------')
+    print(' ' + table[7] + ' | ' + table[8] + ' | ' + table[9])
