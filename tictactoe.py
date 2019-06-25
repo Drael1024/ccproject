548bbb076534bc0e052f34c456ae53524d4cee29
Kuczora Tibor <kuczora.tibi@gmail.com>
2019-06-25 14:32:05 -0700

    Added new function: + isWinner(tableName,letter): Can check a given "table" if there is   a match with any of it's win conditions with the given letter. (X or O)

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,6 @@
 table = [' ' for x in range(10)] # Creating the table for the game. (Note: We won't use the 0th element!!!)
 
-def showTable():
+def showTable(): # Prints out the table.
     print(' ' + table[1] + ' | ' + table[2] + ' | ' + table[3])
     print('-----------')
     print(' ' + table[4] + ' | ' + table[5] + ' | ' + table[6])
@@ -15,3 +15,19 @@ def spaceIsFree(position): # Tells you if it's available to put down your symbol
         return True
     else:
         return False
+
+def isWinner(tableName, letter): # Win condition checker.
+    winCombinations = [
+       (1, 2, 3),
+       (4, 5, 6),
+       (7, 8, 9),
+       (1, 4, 7),
+       (2, 5, 8),
+       (3, 6, 9),
+       (1, 5, 9),
+       (3, 5, 7),
+    ]
+    
+    for i,j,k in winCombinations: # Iterating through the winCombinations, if there's any match, returns true!
+        if (tableName[i] == letter and tableName[j] == letter and tableName[k] == letter):
+            return True
