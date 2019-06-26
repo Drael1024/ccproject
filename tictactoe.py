bd46d25499e9dadd3503c63794ee222ea58861b6
Kuczora Tibor <kuczora.tibi@gmail.com>
2019-06-26 04:12:54 -0700

    Added really important features: + calculatePossibleMoves(): This function returns all the empty fields. + AI(): Has all the basic logics. After calculations, it returns a move. Check the comments in the code for further informations! :)

--- tictactoe.py
+++ tictactoe.py
@@ -54,3 +54,47 @@ def playerTurn(): # :)
                 print('Invalid input! Your number is out of the allowed range!')
         except: # Fool proofing the input. If it can't convert to int, make him redo the procedure!
             print('Invalid input! You must enter a number!')
+
+def calculatePossibleMoves(): # Creates and returns a list which has all the empty (and non-0th) field numbers
+    possibilities = []
+    for x, letter in enumerate(table):
+        if letter == ' ' and x != 0:
+            possibilities.append(x)
+    return possibilities
+
+def AI():
+    possibleMoves = calculatePossibleMoves() # Getting "possible" (empty) positions.
+    move = 0 # This won't be changed if there is no empty positions!! It also means the game ends in a draw! Check the main() function about it.
+
+    for letter in ['O', 'X']: #Trying out moves for both smybols. First the AI tries to win and then tries to "counter-play".
+        for i in possibleMoves:
+            testTable = table[:] #Clones the current table.
+            testTable[i] = letter #Checks all the possible positions.
+            if isWinner(testTable, letter): #If the AI puts down an O and wins in a test table, then it's the correct move! Also tries with "X" to see if the player wins, so at the computer's turn, the AI going to put an "O" there to counter-play!
+                move = i
+                return move
+
+    emptyCorners = [] # Next priorities are the corners.
+    for i in possibleMoves: 
+        if i in [1,3,7,9]:
+            emptyCorners.append(i) 
+
+    if len(emptyCorners) > 0: # Only if it's possible to put a symbol there.
+        r = random.randrange(0,len(emptyCorners))
+        move = emptyCorners[r]
+        return move
+
+    if 5 in possibleMoves: # Next priority is the center square, if possible.
+        move = 5
+        return move
+
+    emptyEdges = [] # The last priorities are the edges.
+    for i in possibleMoves:
+        if i in [2,4,6,8]:
+            emptyEdges.append(i)
+            
+    if len(emptyEdges) > 0: # Only if it's possible to put a symbol.
+        r = random.randrange(0,len(emptyEdges))
+        move = emptyEdges[r]
+        
+    return move # When there are no possibilites left, the AI puts a symbol at the 0th index! It means the game is over and it's a draw!
